0cc4df63d490ae7848675335516c2000457ebf04
fczhao(赵奉辰) <fczhao@tencent.com>
2020-07-03 12:02:01 +0800

    Revert "66 构建乘积数组"
    This reverts commit 06a12579

--- src/Solution.java
+++ src/Solution.java
@@ -1,38 +1,24 @@
-import java.util.Arrays;
 
 class Solution {
-    public static void main(String[] args) {
-        int[] arr = new Solution().constructArr(new int[]{1,2,3,4,5,});
-        System.out.println(Arrays.toString(arr));
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root==null || q==null || p==null) return null;
+        if (p.val > q.val) {
+            return dfs(root,q.val,p.val);
+        }else
+            return dfs(root,p.val,q.val);
     }
-    public int[] constructArr(int[] a) {
-        if (a==null || a.length==0) return null;
-        int len = a.length;
-        int[] forward = new int[len];
-        int[] reverse = new int[len];
+    TreeNode dfs (TreeNode root ,int p ,int q){
+        if (root == null) return null;
 
-        forward[0] = a[0];
-        reverse[len-1] = a[len-1];
-        reverse[0] = a[len-1];
+        TreeNode left =null,right=null;
 
-        for(int q=1;q<len;q++){
-            forward[q] = forward[q-1] * a[q];
+        if (p>=root.val) right = dfs(root.right,p,q);
+        else if (q<=root.val){
+            left = dfs(root.left,p,q);
+        }else {
+            return root;
         }
-        for(int q=len-2;q>=0;q--){
-            reverse[q] = reverse[q+1] * a[q];
-            System.out.println(reverse[q+1] +" aa " + a[q]);
-        }
-
-        int[] arr = new int[len];
-
-        System.out.println("forward "+Arrays.toString(forward));
-        System.out.println("reverse  "+Arrays.toString(reverse));
-        arr[0] = reverse[1];
-        arr[len-1] = forward[len-2];
-        for(int q=1;q<len-1;q++ ){
-            arr[q] = forward[q-1] * reverse[q+1];
-        }
-
-        return arr;
+        return left==null?right:left;
     }
+
 }
